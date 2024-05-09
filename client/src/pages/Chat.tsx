6dfdb2cc5f2b4a74a5699e4286999761bbf0d7fb
Neo Boikano Makoa <boikanomakoa30@gmail.com>
2024-05-09 22:46:10 +0200

    wip(chats)

--- client/src/pages/Chat.tsx
+++ client/src/pages/Chat.tsx
@@ -1,9 +1,128 @@
-import React from 'react'
+import React from "react";
+import { Box, Avatar, Typography, Button } from "@mui/material";
+import red from "@mui/material/colors/red";
 
+import { useAuth } from "../context/AuthContext";
+const chatMessages = [
+  {
+    role: "user",
+    content: "Hello, can you tell me the weather forcast for tomorrow?",
+  },
+  {
+    role: "assistant",
+    content:
+      "Sure! I can help with that. Please provide me with your location.",
+  },
+  {
+    role: "user",
+    content: "I'm in New York City.",
+  },
+  {
+    role: "assistant",
+    content:
+      "Great! Give me a moment to fetch the weather information for New York City.",
+  },
+];
 const Chat = () => {
+  const auth = useAuth();
   return (
-    <div>Chat</div>
-  )
-}
+    <Box
+      sx={{
+        display: "flex",
+        flex: 1,
+        width: "100%",
+        height: "100%",
+        mt: 3,
+        gap: 3,
+      }}
+    >
+      <Box
+        sx={{
+          display: { md: "flex", xs: "none", sm: "none" },
+          flex: 0.2,
+          flexDirection: "column",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            width: "100%",
+            height: "60vh",
+            bgcolor: "rgb(17,29,39)",
+            borderRadius: 5,
+            flexDirection: "column",
+            mx: 3,
+          }}
+        >
+          <Avatar
+            sx={{
+              mx: "auto",
+              my: 2,
+              bgcolor: "white",
+              color: "black",
+              fontWeight: 700,
+            }}
+          >
+            {auth?.user?.name[0]}
+            {auth?.user?.name.split(" ")[1][0]}
+          </Avatar>
+          <Typography sx={{ mx: "auto", fontFamily: "work sans" }}>
+            You are talking to a ChatBot
+          </Typography>
+          <Typography sx={{ mx: "auto", fontFamily: "work sans", my: 4, p: 3 }}>
+            You can as some questions related to knowledge, business, advice,
+            education, etc. But avoid sharing personal information
+          </Typography>
+          <Button
+            sx={{
+              width: "200px",
+              my: "auto",
+              color: "white",
+              fontWeight: 700,
+              borderRadius: 3,
+              mx: "auto",
+              bgcolor: red[300],
+              ":hover": { bgcolor: red.A400 },
+            }}
+          >
+            clear conversation
+          </Button>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flex: { md: 0.8, xs: 1, sm: 1 },
+          flexDirection: "column",
+          px: 3,
+        }}
+      >
+        <Typography
+          sx={{ textAlign: "center", fontSize: "40px", color: "white", mb: 2 }}
+        >
+          Model - GPT 3.5 Turbo
+        </Typography>
+        <Box
+          sx={{
+            width: "100%",
+            height: "60vh",
+            borderRadius: 3,
+            mx: "auto",
+            display: "flex",
+            flexDirection: "column",
+            overflow: "scroll",
+            overflowX: "hidden",
+            overflowY: "auto",
+            scrollBehavior: "smooth",
+          }}
+        >
+          {chatMessages.map((chat) => (
+            <>{chat.content}</>
+          ))}
+        </Box>
+      </Box>
+    </Box>
+  );
+};
 
-export default Chat
+export default Chat;
